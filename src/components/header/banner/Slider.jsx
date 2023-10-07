017a9c25a64b212ab02dc57a6b75e899ce51715f
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-10-07 22:41:04 +0600

    swiper slider implemented

--- src/components/header/banner/Slider.jsx
+++ src/components/header/banner/Slider.jsx
@@ -1,7 +1,46 @@
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import conferences from "../../../assets/images/conferences.jpg";
+import hackathon from "../../../assets/images/hackathon.jpg";
+
 const Slider = () => {
   return (
-    <div className="h-[1200px] bg-green-400">
-      <p>banner</p>
+    <div className="">
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        autoplay={true}
+        pagination={{ clickable: true }}
+        modules={[Navigation, Pagination, Autoplay]}
+      >
+        <SwiperSlide>
+          <div
+            style={{
+              backgroundImage: `url(${conferences})`,
+              backgroundPosition: "center center",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+            }}
+            className="h-screen"
+          ></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            style={{
+              backgroundImage: `url(${hackathon})`,
+              backgroundPosition: "center center",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+            }}
+            className="h-screen"
+          ></div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
