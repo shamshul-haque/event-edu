671b49ec3e17cce3c96c797c7d1ce32bb5638672
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-10-07 14:21:35 +0600

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,41 +1,44 @@
-import {
-  BsFacebook,
-  BsGoogle,
-  BsInstagram,
-  BsTwitter,
-  BsYoutube,
-} from "react-icons/bs";
+import { BsFacebook, BsInstagram, BsTwitter, BsYoutube } from "react-icons/bs";
+import { FcGoogle } from "react-icons/fc";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 const Footer = () => {
   return (
     <footer className="footer footer-center p-10 bg-fuchsia-200 mt-10">
-      <div className="grid grid-flow-col gap-4">
-        <Link to="" className="link link-hover">
+      <div className="flex items-center">
+        <img src={logo} alt="logo" className="w-12" />
+        <p className="text-3xl font-bold">
+          <span className="text-red-500">E</span>vent
+          <span className="text-fuchsia-500">E</span>du
+        </p>
+      </div>
+      <div className="flex items-center gap-4">
+        <Link to="" className="link-hover">
           About us
         </Link>
-        <Link to="#" className="link link-hover">
+        <Link to="#" className="link-hover">
           Contact
         </Link>
-        <Link to="#" className="link link-hover">
+        <Link to="#" className="link-hover">
           Jobs
         </Link>
       </div>
-      <div className="grid grid-flow-col gap-4">
+      <div className="flex items-center gap-4">
         <Link to="#">
-          <BsFacebook className="text-2xl" />
+          <BsFacebook className="text-3xl text-blue-600" />
         </Link>
         <Link to="#">
-          <BsGoogle className="text-2xl" />
+          <FcGoogle className="text-4xl text-red-500" />
         </Link>
         <Link to="#">
-          <BsTwitter className="text-2xl" />
+          <BsTwitter className="text-3xl text-blue-600" />
         </Link>
         <Link to="#">
-          <BsInstagram className="text-2xl" />
+          <BsInstagram className="text-3xl text-red-500" />
         </Link>
         <Link to="#">
-          <BsYoutube className="text-2xl" />
+          <BsYoutube className="text-4xl text-red-500" />
         </Link>
       </div>
       <p>Copyright © 2023 - All right reserved by Shamshul Haque</p>
