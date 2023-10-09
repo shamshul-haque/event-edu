a445a4d41214d9216202ca5af665be2d32ffb6e4
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-10-09 13:39:40 +0600

    update price into items

--- src/components/home/services/ServiceItem.jsx
+++ src/components/home/services/ServiceItem.jsx
@@ -2,16 +2,17 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const ServiceItem = ({ service }) => {
-  const { id, image, name, short_description, button_text } = service;
+  const { id, image, name, short_description, price, button_text } = service;
 
   return (
     <div className="bg-white shadow-lg rounded ">
       <img src={image} alt={name} className="h-52 w-full rounded" />
       <div className="text-center p-5">
         <h1 className="font-bold text-2xl">{name}</h1>
-        <p className="py-3">{short_description}</p>
-        <Link to={`/serviceDetails/${id}`}>
-          <button className="px-4 py-2 bg-red-500 hover:bg-transparent hover:border hover: border-red-500 hover:text-black transition-all duration-300 rounded uppercase text-white font-medium">
+        <p className="pt-3">{short_description}</p>
+        <p className="pt-3 text-3xl">{price}</p>
+        <Link to={`/serviceDetails/${id}`} className="">
+          <button className="px-4 py-2 bg-red-500 hover:bg-transparent hover:border hover: border-red-500 hover:text-black transition-all duration-300 rounded uppercase text-white font-medium mt-4">
             {button_text}
           </button>
         </Link>
